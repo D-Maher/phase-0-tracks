11763777ee1622b77ab8c9245854ce054678db75
Danny Maher <dmaher724@gmail.com>
2016-10-26 10:45:25 -0500

    Added employee screening questions with space for user input

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you? What year were you born?"
+print "Age:"
+age = gets.chomp
+print "Year born:"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order you some?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
