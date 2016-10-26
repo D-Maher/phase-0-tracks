cd8c335dbe2e3a32ff7d5511ac994a58f4b92681
Danny Maher <dmaher724@gmail.com>
2016-10-26 11:05:23 -0500

    Changed gets.chomp to gets.chomp.to_i to convert user's age and year answer to integers, completed first two steps of vampire detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,13 +2,20 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you? What year were you born?"
-print "Age:"
-age = gets.chomp
-print "Year born:"
-birth_year = gets.chomp
+print "Age: "
+age = gets.chomp.to_i
+print "Year born: "
+birth_year = gets.chomp.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order you some?"
 garlic = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
+
+
+if age == 2016 - birth_year && (garlic == "yes" || insurance == "yes")
+  result = "Probably not a vampire."
+elsif age != 2016 - birth_year && (garlic == "no" || insurance == "no")
+  result = "Probably a vampire."
+end
