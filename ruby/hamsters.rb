d70abb242ba032e8ee5bebc6d6f78902b9264a2a
Danny Maher <dmaher724@gmail.com>
2016-10-26 20:09:11 -0500

    Refactored adoption candidate question, added until loop to handle invalid input

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -8,3 +8,14 @@ until (1..10).include?(volume)
   puts "Please select a number between 1 and 10."
   volume = gets.chomp.to_i
 end
+
+puts "What color is the hamster's fur?"
+color = gets.chomp
+
+puts "Is this hamster a good candidate for adoption? (yes/no)"
+candidate = gets.chomp
+
+until candidate == "yes" || candidate == "no"
+  puts "Error. Please type 'yes' or 'no'."
+  candidate = gets.chomp
+end
