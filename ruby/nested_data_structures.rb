3e3ca6ebbe45f0f1fadbc265b05ab808f8f71ab5
Danny Maher <dmaher724@gmail.com>
2016-11-01 13:41:37 -0500

    Added primate_exhibit hash to zoo hash, added several data structures (some nested) within primate_exhibit hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,3 +1,22 @@
 zoo = {
-  
+  primate_exhibit: {
+    gorillas: [
+      "Harambe"
+      "Koko"
+      "Turk"
+    ]
+    chimpanzees: [
+      "Paul"
+      "John"
+      "George"
+      "Ringo"
+    ]
+    fun_facts: {
+      date_built: 1992
+      days_since_incident: 724
+      chimpanzee_rivalries: [
+        "Paul vs Ringo"
+      ]
+    }
+  }
 }
