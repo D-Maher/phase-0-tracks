4fd365f382af0ae8e8c91b728e1f9f1ad84433cf
Danny Maher <dmaher724@gmail.com>
2016-11-01 14:49:10 -0500

    Added another method animals that take a block

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,15 @@
-# a method that takes a block
+# methods that takes a block
 def ice_cream
   yield("vanilla", "strawberry")
 end
 
-ice_cream { |flavor1, flavor2| puts "Two ice cream flavors I enjoy are #{flavor1} and #{flavor2}."}
+ice_cream { |flavor1, flavor2| puts "Two ice cream flavors I enjoy are #{flavor1} and #{flavor2}." }
+
+def animals
+  x = "platypus"
+  y = "wolf"
+  z = "sloth"
+  yield(x, y, z)
+end
+
+animals { |x, y, z| puts "A few of my favorite animals are #{x}, #{y}, and #{z}." }
