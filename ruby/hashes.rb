0924029e773509ddf16eb3440a0e81139ff87bbb
Danny Maher <dmaher724@gmail.com>
2016-10-31 22:39:09 -0500

    Added pseudocode to hashes.rb

---
+++ ruby/hashes.rb
@@ -0,0 +1,24 @@
+# PSEUDOCODE
+
+# prompt user for following info:
+  # name
+  # age
+  # number of children
+  # decor theme
+  # lover of taxidermy?
+
+# convert user input to appropriate data types (from strings to whatever)
+
+# store data in a hash
+
+# print hash to console after all questions have been answered
+
+# give user one opportunity to update the value of a key
+  # if user says "none", skip this step
+  # otherwise, ask user for a key they want to update then the new value for that key
+    # then update the key with this new value
+
+# lastly, print latest version of hash
+
+#---------------------------------------------------------------------
+
