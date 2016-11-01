96b21e6cef2c2c7558d0fca6ff24a609459fa4cb
Danny Maher <dmaher724@gmail.com>
2016-11-01 14:05:42 -0500

    Added commas where appropriate within the zoo hash (I forgot all of them), added driver code to access last chimpanzee rivalry listed in the chimpanzee_rivalries array

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,58 +1,67 @@
+# Hash Setup
+
 zoo = {
   primate_exhibit: {
     gorillas: [
-      "Harambe"
-      "Koko"
+      "Harambe",
+      "Koko",
       "Turk"
-    ]
+    ],
     chimpanzees: [
-      "Paul"
-      "John"
-      "George"
+      "Paul",
+      "John",
+      "George",
       "Ringo"
-    ]
+    ],
     fun_facts: {
-      date_built: 1992
-      days_since_incident: 724
+      date_built: 1992,
+      days_since_incident: 724,
       primate_rivalries: {
         gorilla_rivalries: [
           "Koko vs Turk"
-        ]
+        ],
         chimpanzee_rivalries: [
-        "Paul vs John"
-        ]
+        "Paul vs John",
+        "George vs Ringo"
+        ],
       }
     }
-  }
+  },
 
   aquatic_exhibit: {
     fish: {
       sharks: [
-        "Great White Shark"
-        "Hammerhead Shark"
+        "Great White Shark",
+        "Hammerhead Shark",
         "Mako Shark"
-      ]
+      ],
       rays: [
-        "Manta Ray"
+        "Manta Ray",
         "Stingray"
-      ]
+      ],
       other_fish: [
-        "Tuna"
-        "Coelacanth"
+        "Tuna",
+        "Coelacanth",
         "Blue Tang"
-      ]
-    }
+      ],
+    },
     mammals: [
-      "Dolphin"
-      "Platypus"
-      "Humpback Whale"
-    ]
+      "Dolphin",
+      "Platypus",
+      "Humpback Whale",
+    ],
     dolphin_show: {
-      start_time: "3:00pm"
-      end_time: "4:00pm"
-      seats_available: 500
+      start_time: "3:00pm",
+      end_time: "4:00pm",
+      seats_available: 500,
       average_daily_attendance: 421
-    }
+    },
   }
 
 }
+
+
+# Driver Code
+
+# Access last chimpanzee rivalry in the list
+puts zoo[:primate_exhibit][:fun_facts][:primate_rivalries][:chimpanzee_rivalries].last
