9fc78e589be0f11deaf54f6320e26eb41f08e1c2
Danny Maher <dmaher724@gmail.com>
2016-11-01 13:56:15 -0500

    Added aquatic_exhibit hash to zoo hash containing several nested data structures and items

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -15,8 +15,39 @@ zoo = {
       date_built: 1992
       days_since_incident: 724
       chimpanzee_rivalries: [
-        "Paul vs Ringo"
+        "Paul vs John"
       ]
     }
   }
+
+  aquatic_exhibit: {
+    fish: {
+      sharks: [
+        "Great White Shark"
+        "Hammerhead Shark"
+        "Mako Shark"
+      ]
+      rays: [
+        "Manta Ray"
+        "Stingray"
+      ]
+      other_fish: [
+        "Tuna"
+        "Coelacanth"
+        "Blue Tang"
+      ]
+    }
+    mammals: [
+      "Dolphin"
+      "Platypus"
+      "Humpback Whale"
+    ]
+    dolphin_show: {
+      start_time: "3:00pm"
+      end_time: "4:00pm"
+      seats_available: 500
+      average_daily_attendance: 421
+    }
+  }
+
 }
