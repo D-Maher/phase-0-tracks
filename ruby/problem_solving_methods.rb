5206119d5a448e9ca237a9f85f0ada2f1dcb7518
Danny Maher <dmaher724@gmail.com>
2016-11-03 11:45:09 -0500

    Added some pseudocode and brainstormed a few ideas

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,19 @@
+# RELEASE 0
+
+# PSEUDOCODE
+
+# Method that takes an array of integers and an integer to search for within that array
+  # If integer is inside the array, return the index of the integer
+    # i.e. if array 
+  # Otherwise return 'nil'
+
+def search_array(array, integer)
+  while array.length <
+
+
+array.each do |item|
+  if array.include?(integer)
+
+
+(array.length).times do
+  
