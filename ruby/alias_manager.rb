302957f48c95b779ed685ec0acf299f4bae2b752
Danny Maher <dmaher724@gmail.com>
2016-11-03 16:14:44 -0500

    Added pseudocode for an alias_maker method

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,15 @@
+# PSEUDOCODE
+
+# alias method
+  # swap the first and last name
+
+  # change all of the vowels (a, e, i, o, u) to the next vowel (e, i, o, u, a)
+    # if vowel, change to next vowel (a => e, e => i, etc)
+
+  # change all of the consonants to the next consonant
+    # if consonant, change to next consonant (b => c, c => d, d => f, etc)
+
+  #-------------------------------------------------------------------
+
+def alias_maker(full_name)
+  full_name
