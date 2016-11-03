df8737b5dc9b70f8409e4ecc58ffa35500444dd9
Danny Maher <dmaher724@gmail.com>
2016-11-03 16:21:37 -0500

    Added a little advance pseudocode for user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,13 +1,17 @@
 # PSEUDOCODE
 
-# alias method
-  # swap the first and last name
+  # alias method
+    # swap the first and last name
 
-  # change all of the vowels (a, e, i, o, u) to the next vowel (e, i, o, u, a)
-    # if vowel, change to next vowel (a => e, e => i, etc)
+    # change all of the vowels (a, e, i, o, u) to the next vowel (e, i, o, u, a)
+      # if vowel, change to next vowel (a => e, e => i, etc)
 
-  # change all of the consonants to the next consonant
-    # if consonant, change to next consonant (b => c, c => d, d => f, etc)
+    # change all of the consonants to the next consonant
+      # if consonant, change to next consonant (b => c, c => d, d => f, etc)
+
+  # user interface
+    # convert user input to lowercase right when obtained
+    # capitalize later
 
   #-------------------------------------------------------------------
 
@@ -17,4 +21,3 @@ def alias_maker(full_name)
   p reverse_name
 end
 
-alias_maker("Danny Maher")
