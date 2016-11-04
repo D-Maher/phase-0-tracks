42ea165c812f9bc3b401895179b60817010458ed
Danny Maher <dmaher724@gmail.com>
2016-11-03 20:13:52 -0500

    Massive overhaul of code: divided overall alias_maker method into component methods swap_names and split_names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -15,47 +15,63 @@
 
   #-------------------------------------------------------------------
 
-def swap_names(first_last)
-  last_first = first_last.split(' ').reverse # returns array of ["last name", "first name"]
+vowels = "aeiou".split('') # an array of all vowels in alpha order
+consonants = "bcdfghjklmnpqrstvwxyz".split('') # an array of all consonants in alpha order
+
+def swap_names(given_name)
+  last_first = given_name.split(' ').reverse # returns array of ["last name", "first name"]
+  last_first
 end
 
-def alias_maker(first_last)
-  index = 0
+def split_names(last_name, first_name)
+  last_name_split = last_name.split('')
+  first_name_split = first_name.split('')
 
-  vowels = "aeiou".split('')
-  consonants = "bcdfghjklmnpqrstvwxyz".split('')
-
-  last_first = swap_names(first_last)
-
-  alias_first_letters = last_first[0].split('')
-
-  alias_last_letters = last_first[1].split('')
-
-  alias_first_letters.each do |letter|
-    letter = letter.downcase
-    if vowels.include?(letter)
-      p letter
-      # vowel_index = vowels.index(alias_first_letters[index])
-      # new_vowel = vowels[vowel_index].next
-      # p new_vowel
-    # elsif consonants.include(letter)
-    #   letter = 
-    elsif consonants.include?(letter)
-      p letter
-    end
-
-  end
-
-  alias_last_letters.each do |letter|
-    letter = letter.downcase
-    if vowels.include?(letter)
-      p letter
-    elsif consonants.include?(letter)
-      p letter
-    end
-  end
-  
+  p last_name_split
+  p first_name_split
 end
 
+def alias_maker(name)
+  swap_array = swap_names(name)
+  split_names(swap_array[0], swap_array[1])
+end
 
 alias_maker("Danny Maher")
+
+              # def next_vowel(letter)
+              #   index = 0
+              #   while index < alias_first_letters.length
+              #     if vowels.include?(alias_first_letters[index])
+              #       alias_first_letters[index] = vowels.index(alias_first_letters[index])
+              #     else
+              #     end
+              #   end
+              #   index += 1
+              #   alias_first_letters[index]
+              # end
+
+
+
+              #   last_first = swap_names(first_last)
+
+              #   alias_first_letters = last_first[0].split('')
+
+              #   alias_last_letters = last_first[1].split('')
+
+              # def alias_maker(first_last)
+              #   index = 0
+              #   alias_first_letters.each do |letter|
+              #     letter = letter.downcase
+              #     p next_vowel(letter)
+              #   end
+
+              #   alias_last_letters.each do |letter|
+              #     letter = letter.downcase
+              #     if vowels.include?(letter)
+              #       p letter
+              #     elsif consonants.include?(letter)
+              #       p letter
+              #     end
+              #   end
+                
+              # end
