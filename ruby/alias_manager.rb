1d61dcf72801b5fe3fdb273f933d65dd946d45d8
Danny Maher <dmaher724@gmail.com>
2016-11-03 21:54:28 -0500

    Added method next_vowel and verified that it works as expected, began setup of next_consonant method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,34 +13,51 @@
     # convert user input to lowercase right when obtained
     # capitalize later
 
-  #-------------------------------------------------------------------
+  #------------------------------------------------------------------
+
+# In all code below, first_name represents the REAL first name of the agent, and last_name represents the REAL last name of the agent.
 
 $vowels = "aeiou".split('') # an array of all vowels in alpha order
 $consonants = "bcdfghjklmnpqrstvwxyz".split('') # an array of all consonants in alpha order
 
-def swap_names(given_name)
+def swap_names(given_name) # WORKS!
   last_first_array = given_name.split(' ').reverse # returns array of ["last name", "first name"]
   last_first_array
 end
 
-def split_names(name_array)
-  name_letters = name_array.split('')
+def split_names(name_string) # WORKS!
+  name_letters = name_string.split('')
   name_letters.each { |letter| letter.downcase }
-  p name_letters
+  name_letters
 end
 
-def next_vowel
-  # $vowels.include?
+def next_vowel(letters_array) # WORKS!
+  index = 0
+  while index < letters_array.length
+    if $vowels.include?(letters_array[index]) # if letter is a vowel
+      vowel_index = $vowels.index(letters_array[index])
+      letters_array[index] = $vowels[vowel_index.next]
+      p letters_array
+    end
+    index += 1
+  end
 end
 
-
+def next_consonant
+end
 
 
 def alias_maker(name)
   swap_array = swap_names(name)
-  # last_name_letters = split_names(swap_array[0]). 
-  # first_name_letters = split_names(swap_array[1])
-  next_vowel
+
+  last_name_letters = split_names(swap_array[0]) 
+  first_name_letters = split_names(swap_array[1])
+  
+
+  next_vowel(last_name_letters)
+  next_vowel(first_name_letters)
+
+
 end
 
 alias_maker("Danny Maher")
