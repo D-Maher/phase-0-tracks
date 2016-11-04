8158963a9e7140c055aee39ba35eeb5fec62ddf8
Danny Maher <dmaher724@gmail.com>
2016-11-04 14:53:58 -0500

    Began to add method to search an array of integers for a particular integer

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -2,18 +2,28 @@
 
 # PSEUDOCODE
 
-# Method that takes an array of integers and an integer to search for within that array
-  # If integer is inside the array, return the index of the integer
-    # i.e. if array 
-  # Otherwise return 'nil'
+  # Search method that takes an array of integers and an integer to search for within that array
+    # If integer is inside the array, return the index of the integer
+      # i.e. if array 
+    # Otherwise return 'nil'
+
+
+
+# --------------------------------------------------------------------
 
 def search_array(array, integer)
-  while array.length <
+  index = 0
+  while index < array.length
+    if array.include?(integer)
+      index
+    else
+      nil
+    end
+    index += 1
+  end
+  index 
+end
 
+array_for_search = [2, 7, 55, 13, 24, 17, 9]
 
-array.each do |item|
-  if array.include?(integer)
-
-
-(array.length).times do
-  
+p search_array(array_for_search, 24)
