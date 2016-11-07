3e345b579c79501a293c76faf4d852748eff94aa
Danny Maher <dmaher724@gmail.com>
2016-11-07 12:53:01 -0600

    Added design for Puppy class in comments at top of puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+#  Puppy class design:
+  
+  # SPECIES
+    # Canis familiaris
+
+  # CHARACTERISTICS
+    # Friendly: absolutely
+    # Legs: 4
+    # Fur: varies
+    # Color: varies
+    # Name: something cute
+    # Desirable as pet: yes
+
+  # BEHAVIOR
+    # Tail wag
+    # Fetch
+    # Run
+    # Sit
+    # Shake paw
