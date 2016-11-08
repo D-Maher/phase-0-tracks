e1f81d1b5f98359f42c26b52ae20704d2363ea8b
Danny Maher <dmaher724@gmail.com>
2016-11-08 15:44:12 -0600

    Set instance variable @name equal to name, a parameter of the initialize method for class Reindeer, updated driver code accordingly with 'Blitzen' as the argument

--- ruby/practice_reindeer.rb
+++ ruby/practice_reindeer.rb
@@ -4,9 +4,9 @@
 
 class Reindeer
 
-  def initialize
-    @name = "Rudolph" # this instance variable's data is accessible to any instance methods of the Reindeer class
-    @location = "the North Pole" # same with this instance variable
+  def initialize(name) # initialize method now takes a parameter 'name'
+    @name = name # this instance variable's data is accessible to any instance methods of the Reindeer class
+    @location = "the North Pole" # same with this instance variable # we leave "the North Pole" as the reindeer's default location, which we can update later in the code
   end
 
   def take_off(altitude)
@@ -20,6 +20,6 @@ class Reindeer
 
 end
 
-reindeer = Reindeer.new
+reindeer = Reindeer.new("Blitzen")
 reindeer.take_off(3000)
 reindeer.land("Dublin")
