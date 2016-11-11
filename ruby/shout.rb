499998ea814393ca85ee6094258eda670c04eac6
Danny Maher <dmaher724@gmail.com>
2016-11-11 16:14:18 -0600

    Commented out old 'Shout' module code, added 'Shout' mixin declaration with instance methods 'yell_angrily' and 'yell_happily', included 'Shout' in classes 'SportsFan' and 'SentientGoat'

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,20 +1,37 @@
 # BUSINESS LOGIC
 
-module Shout
-  
-  def self.yell_angrily(words)
-    words + "!!!" + " >:("
+  # module Shout # plain module version (note 'self' keyword)
+    
+  #   def self.yell_angrily(words)
+  #     words + "!!!" + " >:("
+  #   end
+
+  #   def self.yell_happily(words)
+  #     words + "!!!" + " :)"
+  #   end
+
+  # end
+
+module Shout # mixin version (note lack of 'self' keyword)
+
+  def yell_angrily(words)
+      words + "!!!" + " >:("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     words + "!!!" + " :)"
   end
 
 end
 
+class SportsFan
+  include Shout
+end
+
+class SentientGoat
+  include Shout
+end
+
 
 # DRIVER CODE
 
-p Shout.yell_angrily("Ruby is frustrating")
-
-p Shout.yell_happily("Ruby is fun")
