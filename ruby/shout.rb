13c2657cf4afe40ad3a5328f27227da27f95b58c
Danny Maher <dmaher724@gmail.com>
2016-11-11 15:47:49 -0600

    Added method 'self.yell_happily' to module 'Shout', added driver code to verify that 'yell_angrily' and 'yell_happily' run as expected

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,7 +1,20 @@
+# BUSINESS LOGIC
+
 module Shout
   
   def self.yell_angrily(words)
     words + "!!!" + " >:("
   end
 
+  def self.yell_happily(words)
+    words + "!!!" + " :)"
+  end
+
 end
+
+
+# DRIVER CODE
+
+p Shout.yell_angrily("Ruby is frustrating")
+
+p Shout.yell_happily("Ruby is fun")
