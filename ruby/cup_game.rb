ee106ae41030d2b4d46ee17024d8a633f60d0df8
Danny Maher <dmaher724@gmail.com>
2016-11-12 13:21:52 -0600

    Added a few more comments to cup_game.rb

--- ruby/cup_game.rb
+++ ruby/cup_game.rb
@@ -19,7 +19,7 @@ class CupGame
     if @cups[index] == "ball" # if the user's guess - 1 is equal to the index of the item "ball" within the @cups array...
       @is_over = true # ...then the game ends
     else
-      false # otherwise the game continues
+      false # ...otherwise the game continues
     end
   end
 
@@ -37,7 +37,7 @@ while !game.is_over # while the game is not over... (another way to write this c
   puts "Which cup has the ball? Enter a guess of 1, 2, or 3:"
   guess = gets.chomp.to_i # asks for user input and converts it to an integer
   if !game.check_cup(guess - 1) # if the user's guess - 1 does not equal the index of "ball" in the array @cups (i.e. if game.check_cup evaluates to false since the 'if' statement was not met)
-    puts "Nope! Try again."
+    puts "Nope! Try again." # user has not guessed correctly, 'while' loop continues
   end
 end
 
