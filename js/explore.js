560fcced852689665e60c62b9a323a197c7af796
Danny Maher <dmaher724@gmail.com>
2016-11-15 12:33:56 -0600

    Added function 'reverse', added driver code to test function on a string

--- js/explore.js
+++ js/explore.js
@@ -6,3 +6,13 @@
     // for each letter in the string...
       // assign letter to -(index) of that letter
   // output: the string given to the function, reversed ("gnirts")
+
+function reverse(string) {
+  var reversedString = "";
+  for (var i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i]
+  }
+  console.log(reversedString);
+}
+
+reverse("hello")
