c233d64f47a1f8a8874522747c2f646e54f73736
Danny Maher <dmaher724@gmail.com>
2016-11-15 12:15:06 -0600

    Added pseudocode for a 'reverse' function in explore.js

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// PSEUDOCODE
+
+// function that takes a string as a parameter and reverses string
+  // input: a "string"
+  // steps:
+    // for each letter in the string...
+      // assign letter to -(index) of that letter
+  // output: the string given to the function, reversed ("gnirts")
