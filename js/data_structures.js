f14e9148f27f23913266523f5c6f0d1591a3de5d
Danny Maher <dmaher724@gmail.com>
2016-11-16 15:51:40 -0600

    Added 'purple' to horseColors array, added 'Jim' to horseNames array

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,18 @@
-//  Release 0
+// Release 0
 
 var horseColors = ["blue", "red", "yellow", "green"];
 
-var horseNames = ["Ed", "Bob", "Bill", "Joe"]
+var horseNames = ["Ed", "Bob", "Bill", "Joe"];
+
+console.log(horseColors);
+
+console.log(horseNames);
+
+
+horseColors.push("purple");
+
+horseNames.push("Jim");
+
+console.log(horseColors);
+
+console.log(horseNames);
