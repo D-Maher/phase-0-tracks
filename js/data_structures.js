f342580e457c111fd3544662348df1468a6093bc
Danny Maher <dmaher724@gmail.com>
2016-11-16 16:40:41 -0600

    Added constructor function 'Car' to build cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -25,3 +25,21 @@ var horse = { horseNames: horseColors };
 for (var i = 0; i < horseNames.length; i++) {
   console.log(horseNames[i] + " is " + horse.horseNames[i]);
 }
+
+
+// Release 2
+
+function Car(make, model, year, isGoodCar) {
+  console.log("Our new car: ", this);
+
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.isGoodCar = isGoodCar;
+
+  this.vroom = function() { console.log("*VROOOOMMM!!!*"); }
+  ;
+
+  console.log("CAR INITIALIZATION COMPLETE");
+
+}
