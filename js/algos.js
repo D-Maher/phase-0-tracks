6fb1c51aa2f912fc2df43ccaf5ffbd2cfa185755
Danny Maher <dmaher724@gmail.com>
2016-11-20 12:57:39 -0600

    Added pseudocode for a 'findLongest' function

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// Release 0: Find the Longest Phrase
+
+// PSEUDO
+  // declare findLongest function
+    // input: array of words/phrases
+    // steps:
+      // - isolate each word/phrase in the array by index
+      // - find the length of each word or phrase
+      // - for each adjacent pair in the array, place the longer word/phrase first in the array, doing so until all words are sorted from longest to shortest (bubble sort)
+      // return the first word/phrase in the array
+    // output: the longest phrase in the inputted array
