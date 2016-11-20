1e8f26bc52c3f4f211be09d207585a8bb536023d
Danny Maher <dmaher724@gmail.com>
2016-11-20 14:28:46 -0600

    Added function 'findLongest' to 'algos.js', added corresponding driver code to test 'findLongest' on an array of words

--- js/algos.js
+++ js/algos.js
@@ -4,8 +4,37 @@
   // declare findLongest function
     // input: array of words/phrases
     // steps:
-      // - isolate each word/phrase in the array by index
-      // - find the length of each word or phrase
-      // - for each adjacent pair in the array, place the longer word/phrase first in the array, doing so until all words are sorted from longest to shortest (bubble sort)
-      // return the first word/phrase in the array
+      // - find the length of each word or phrase and add that length to a new array of word/phrase lengths (integers)
+      // - sort the array of lengths from largest to smallest using bubble sort
+        // - swapping adjacent lengths if one length is less than the next length
+      // return the word/phrase in the initial array whose length matches the first integer in the lengths array
     // output: the longest phrase in the inputted array
+
+var words = ["word", "woord", "wooooooorrrrrd", "wooooord", "word"];
+
+var wordLengths = [];
+
+function findLongest(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    wordLengths.push(arr[i].length);
+  }
+  var swapped;
+  do {
+    swapped = false;
+    for (var j = 0; j < wordLengths.length - 1; j++) {
+      if (wordLengths[j] < wordLengths[j + 1]) {
+        var temp = wordLengths[j + 1];
+        wordLengths[j + 1] = wordLengths[j];
+        wordLengths[j] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  for (var k = 0; k < arr.length; k++) {
+    if (arr[k].length == wordLengths[0]) {
+      console.log(arr[k]);
+    }
+  }
+}
+
+findLongest(words)
