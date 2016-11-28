ec64b4185d8ca9ea14e36a38d4dc28f3f70e7eaf
Danny Maher <dmaher724@gmail.com>
2016-11-27 21:00:12 -0600

    Added some pseudocode to beer_rater.rb

--- databases/Beer Rater/beer_rater.rb
+++ databases/Beer Rater/beer_rater.rb
@@ -1,3 +1,14 @@
 # PSEUDOCODE
 
 # Program description: an "app" that lets the user keep track of different beers, as well as that user's rating for each beer, then stores that data in a database
+
+# initialize a database
+  # create a 'beers' table to hold each beer and its corresponding rating
+  # create a 'users' table to keep track of users that log in
+
+# setup main menu with the following options for the user:
+  # - add a beer with its rating
+  # - update a beer's rating
+  # - remove a beer from the list
+  # - display all beers with their ratings
+  # - quit the program
