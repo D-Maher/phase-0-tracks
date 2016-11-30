109dc909635878426fa48c80a027dad8013d2b3e
Danny Maher <dmaher724@gmail.com>
2016-11-30 15:34:59 -0600

    Comment out non-DRY function 'addPinkBorder()'

--- web_dev/chameleon/script.js
+++ web_dev/chameleon/script.js
@@ -1,5 +1,11 @@
 console.log("The script is running!");
 
+
+// function addPinkBorder() { // this is refactored below
+//   var photo = document.getElementById("lizard-photo");
+//   photo.style.border = "2px solid pink";
+// }
+
 function addPinkBorder(event) {
   console.log("click happened! here's the click event:");
   console.log(event);
