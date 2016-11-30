a1b45758f53abddcc92a78b701a6c4d3b207709a
Danny Maher <dmaher724@gmail.com>
2016-11-30 16:53:21 -0600

    Add function 'blowUpUnrotateImage(event)' to enlarge and 'unrotate' image on index.html, add corresponding EventListener to the image to perform the function 'blowUpUnrotateImage' when the user double-clicks the image

--- web_dev/client_side_js/script_project.js
+++ web_dev/client_side_js/script_project.js
@@ -1,8 +1,17 @@
+var mainImage = document.getElementById("main-image-wrap").firstElementChild;
 
 function shrinkRotateImage() {
-  var mainImage = document.getElementById("main-image-wrap").firstElementChild;
   mainImage.style.transform = "rotate(-10deg)";
   mainImage.style.width = "20%";
 }
 
 shrinkRotateImage();
+
+
+function blowUpUnrotateImage(event) {
+  event.target.parentElement.style.width = "90%";
+  event.target.style.width = "100%"
+  event.target.style.transform = "rotate(0deg)"
+}
+
+mainImage.addEventListener("dblclick", blowUpUnrotateImage);
